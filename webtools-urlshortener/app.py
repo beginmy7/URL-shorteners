14e52917f49c84d89534c43bb4da9b2d5808061e
beginmy7 <145587308+beginmy7@users.noreply.github.com>
2023-09-21 07:21:36 +0700

    Update app.py

--- webtools-urlshortener/app.py
+++ webtools-urlshortener/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pyshorteners
 
-st.title("<h1 style= 'text-align: center;'>🔗URL Shortener</h1>", unsafe_allow_html=True)
+st.title("<h1 style= 'text-align: center;'>🔗URL Shortener</h1>")
 st.write("Crafted by Izatcrust ✨")
 st.divider()
 
